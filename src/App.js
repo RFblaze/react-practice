038cb2bd44adcbedcc5d253721ad78bf93c60013
Angelo Troncone <angelotroncone@gmail.com>
2023-07-15 19:39:05 -0600

    Initial test branch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-import { useState } from "react";
-import Alert from "./Alert.js";
-import MyButton from "./MyButton.js";
-
-function App() {
-  const [showAlert, setShowAlert] = useState(false);
-
-  const onClick = (event) => {
-    const { target } = event;
-    const { className } = target;
-    console.log(className);
-    if (className === "btn btn-primary") setShowAlert(true);
-    else if (className === "btn-close") setShowAlert(false);
-  };
-
-  return (
-    <>
-      {showAlert === true ? (
-        <Alert show={showAlert} closeClick={onClick}>
-          My alert
-        </Alert>
-      ) : null}
-      <MyButton type="primary" onClick={onClick}>
-        Hey
-      </MyButton>
-    </>
-  );
-}
+function App() {}
 
 export default App;
--- src/MyButton.js
+++
@@ -1,9 +0,0 @@
-function MyButton({ children, type = "primary", onClick }) {
-  return (
-    <button type={"button"} className={"btn btn-" + type} onClick={onClick}>
-      {children}
-    </button>
-  );
-}
-
-export default MyButton;
